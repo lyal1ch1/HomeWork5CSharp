1d5e4317cac855f5d424fa6a3d9e1031a7b331b8
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-17 20:00:57 +0300

    Add files via upload

---
+++ Zadacha34.cs
@@ -0,0 +1,41 @@
+﻿// Zadacha34
+
+Console.WriteLine("Введите размер массива:");
+int num = int.Parse(Console.ReadLine()!);
+int[] array = new int[num];
+int[] FillArray(int num)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(" " + array[i]);
+    }
+}
+
+int PositiveNumOfArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i] % 2 == 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+FillArray(num);
+PrintArray();
+Console.WriteLine();
+Console.WriteLine("В данном массиве " +  PositiveNumOfArray(array) + " четных чисел");
---
+++ Zadacha36.cs
@@ -0,0 +1,42 @@
+﻿// Zadacha36
+
+Console.WriteLine("Введите размер массива:");
+int length = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальное число массива:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное число массива:");
+int max = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[length];
+int[] FillArray(int length, int min, int max)
+{
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(" " + array[i]);
+    }
+}
+FillArray(length,min,max);
+PrintArray();
+
+int SumNegativeNumOfArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length ; i+= 2)
+    {
+        sum +=array[i]; 
+    }
+    return sum;
+}
+
+Console.WriteLine();
+Console.WriteLine("Сумма нечетных элементов массива равна: " + SumNegativeNumOfArray(array));
